eafaf24808967401a7c0972bd9632f106eb563cc
anin90 <anin90@gmail.com>
2021-10-16 14:10:16 +0530

    Fix bad repo

--- 3.analyzeModel/Analysis_Lithium/plot/lithium_bd_overlap.R
+++ 3.analyzeModel/Analysis_Lithium/plot/lithium_bd_overlap.R
@@ -23,13 +23,18 @@ pdf("lithium_bd_venn.pdf")
 ###########
 # Load data
 ###########
-bd_212_tbl <- read.csv("/media/anirudh/Work/ADBS_NIMHANS/Thesis/1.Science/Analysis/cobratoolbox/AstroModel/3.analyzeModel/Analysis_BD/plot/bd_212_tbl.csv", header = T, sep = "\t")
-bd_r_92_tbl <- read.csv("/media/anirudh/Work/ADBS_NIMHANS/Thesis/1.Science/Analysis/cobratoolbox/AstroModel/3.analyzeModel/Analysis_BD/plot/bd_r_92_tbl.csv", header = T, sep = "\t")		
-bd_nr_670_tbl <- read.csv("/media/anirudh/Work/ADBS_NIMHANS/Thesis/1.Science/Analysis/cobratoolbox/AstroModel/3.analyzeModel/Analysis_BD/plot/bd_nr_670_tbl.csv", header = T, sep = "\t")
 iAstro_Primary_TP <- read.csv("/media/anirudh/Work/ADBS_NIMHANS/Thesis/1.Science/Analysis/cobratoolbox/AstroModel/3.analyzeModel/Analysis_T_TP/Rxns_iAstro_Primary_TP.csv", header = T, sep = "\t")
 iAstro_iPS_BD_TP <- read.csv("/media/anirudh/Work/ADBS_NIMHANS/Thesis/1.Science/Analysis/cobratoolbox/AstroModel/3.analyzeModel/Analysis_T_TP/Rxns_iAstro_iPS_BD_TP.csv", header = T, sep = "\t")
 iAstro_iPS_BD_R_TP <- read.csv("/media/anirudh/Work/ADBS_NIMHANS/Thesis/1.Science/Analysis/cobratoolbox/AstroModel/3.analyzeModel/Analysis_T_TP/Rxns_iAstro_iPS_BD_R_TP.csv", header = T, sep = "\t")
 iAstro_iPS_BD_NR_TP <- read.csv("/media/anirudh/Work/ADBS_NIMHANS/Thesis/1.Science/Analysis/cobratoolbox/AstroModel/3.analyzeModel/Analysis_T_TP/Rxns_iAstro_iPS_BD_NR_TP.csv", header = T, sep = "\t")
+
+bd_212_tbl <- read.csv("/media/anirudh/Work/ADBS_NIMHANS/Thesis/1.Science/Analysis/cobratoolbox/AstroModel/3.analyzeModel/Analysis_BD/plot/bd_212_tbl.csv", header = T, sep = "\t")
+bd_r_92_tbl <- read.csv("/media/anirudh/Work/ADBS_NIMHANS/Thesis/1.Science/Analysis/cobratoolbox/AstroModel/3.analyzeModel/Analysis_BD/plot/bd_r_92_tbl.csv", header = T, sep = "\t")		
+bd_nr_670_tbl <- read.csv("/media/anirudh/Work/ADBS_NIMHANS/Thesis/1.Science/Analysis/cobratoolbox/AstroModel/3.analyzeModel/Analysis_BD/plot/bd_nr_670_tbl.csv", header = T, sep = "\t")
+
+bd_tbl <- read.csv("/media/anirudh/Work/ADBS_NIMHANS/Thesis/1.Science/Analysis/cobratoolbox/AstroModel/3.analyzeModel/Analysis_BD/FSR_iAstro_iPS_Ctrl_TP_vs_iAstro_iPS_BD_TP.csv", header = T, sep = "\t")
+bd_r_tbl <- read.csv("/media/anirudh/Work/ADBS_NIMHANS/Thesis/1.Science/Analysis/cobratoolbox/AstroModel/3.analyzeModel/Analysis_BD/FSR_iAstro_iPS_Ctrl_TP_vs_iAstro_iPS_BD_R_TP.csv", header = T, sep = "\t")		
+bd_nr_tbl <- read.csv("/media/anirudh/Work/ADBS_NIMHANS/Thesis/1.Science/Analysis/cobratoolbox/AstroModel/3.analyzeModel/Analysis_BD/FSR_iAstro_iPS_Ctrl_TP_vs_iAstro_iPS_BD_NR_TP.csv", header = T, sep = "\t")
 
 ###############################################################################################
 # https://stats.stackexchange.com/questions/95523/significance-of-overlap-between-multiple-lists
@@ -72,24 +77,40 @@ Untreated_vs_Lithium_Output <- read.csv("/media/anirudh/Work/ADBS_NIMHANS/Thesis
 Untreated_vs_Lithium_Input <- read.csv("/media/anirudh/Work/ADBS_NIMHANS/Thesis/1.Science/Analysis/cobratoolbox/AstroModel/3.analyzeModel/Analysis_Lithium/iAstro_Primary_TP_Untreated_vs_Lithium_Input.csv",header = T, sep = "\t")
 bind = bind_rows(Untreated_vs_Lithium_Output,Untreated_vs_Lithium_Input)
 Untreated_vs_Lithium_all = coalesce(bind$m_out, bind$m_in)
+#Untreated_vs_Lithium_all = Untreated_vs_Lithium_Input$m_in #InputRxns_only
 
 # Fig.1: (Primary_TP_Akkouh vs BD_Disrupted)
 
-#1a (venn rxns)
-x <- list(
-  Akkouh = Untreated_vs_Lithium_all,	
-  BD_Lumped = bd_212_tbl$rxnList, 
-  BD_R = bd_r_92_tbl$rxnList, 
-  BD_NR = bd_nr_670_tbl$rxnList)
-p = ggvenn(x, fill_color = c("grey","blue", "green", "red")); p
-#svg(filename="lithium_bd_venn.svg"); p; dev.off()
+#1a (venn rxns, akkouh vs bd_212_92_670)
+	x <- list(
+	Akkouh = Untreated_vs_Lithium_all,	
+	BD_Lumped = bd_212_tbl$rxnList, 
+	BD_R = bd_r_92_tbl$rxnList, 
+	BD_NR = bd_nr_670_tbl$rxnList)
+	p = ggvenn(x, fill_color = c("grey","blue", "green", "red")); p
 
-#1b (venn hypergeometric test)
-background <- sum(c(nrow(Untreated_vs_Lithium_all),nrow(bd_212_tbl), nrow(bd_r_92_tbl), nrow(bd_nr_670_tbl)))				
-M <- hyper_matrix(x, 6500);	M[upper.tri(M)] <- NA;	diag(M)<-NA;
-pheatmap(M, cluster_rows=F, cluster_cols=F, na_col="white", display_numbers = TRUE, main = 'Significance of pairwise overlap', 
-	fontsize_number = 20, fontsize = 15)
-#p = pheatmap(M, cluster_rows=F, cluster_cols=F, na_col="white", display_numbers = TRUE)	
-#svg(filename="lithium_bd_hypermat.svg", width=3, height=3); p; dev.off()
+#1b (venn hypergeometric test, akkouh vs bd_212_92_670)
+	y = list(iAstro_Primary_TP$Var1, iAstro_iPS_BD_TP$Var1, iAstro_iPS_BD_R_TP$Var1, iAstro_iPS_BD_NR_TP$Var1);
+	yInt = Reduce(union,y); background = length(yInt);
+	M <- hyper_matrix(x, background);	M[upper.tri(M)] <- NA;	diag(M)<-NA;
+	pheatmap(M, cluster_rows=F, cluster_cols=F, na_col="white", display_numbers = TRUE, main = 'Significance of pairwise overlap', 
+		fontsize_number = 20, fontsize = 15)
+
+#1c (venn rxns, akkouh vs bd_all)
+	x <- list(
+	Akkouh = Untreated_vs_Lithium_all,	
+	BD_Lumped = bd_tbl$m, 
+	BD_R = bd_r_tbl$m, 
+	BD_NR = bd_nr_tbl$m)
+	p = ggvenn(x, fill_color = c("grey","blue", "green", "red")); p
+
+#1d (venn hypergeometric test, akkouh vs bd_all)
+	y = list(iAstro_Primary_TP$Var1, iAstro_iPS_BD_TP$Var1, iAstro_iPS_BD_R_TP$Var1, iAstro_iPS_BD_NR_TP$Var1);
+	yInt = Reduce(union,y); background = length(yInt);
+	M <- hyper_matrix(x, background);	M[upper.tri(M)] <- NA;	diag(M)<-NA;
+	pheatmap(M, cluster_rows=F, cluster_cols=F, na_col="white", display_numbers = TRUE, main = 'Significance of pairwise overlap', 
+		fontsize_number = 20, fontsize = 15)
+
+
 
 
