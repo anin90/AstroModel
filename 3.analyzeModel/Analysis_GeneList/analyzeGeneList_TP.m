f46e3712841b806eea36519a4695b173cc088750
anin90 <anin90@gmail.com>
2022-02-04 02:11:27 +0530

    Fix bad repo

--- 3.analyzeModel/Analysis_GeneList/analyzeGeneList_TP.m
+++ 3.analyzeModel/Analysis_GeneList/analyzeGeneList_TP.m
@@ -84,6 +84,149 @@ FluxDiff.GeneToRxnList = GeneToRxnList;
 FluxDiff_Primary_TP_PLCB4 = FluxDiff;
 clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_Primary_TP_PLCB4
 
+%% NMNAT1 (64802)
+[~, GeneToRxnList, iAstro_Primary_TP_NMNAT1] = downregulateGene(iAstro_Primary_TP, '64802');
+model_a = iAstro_Primary_TP;
+model_b = iAstro_Primary_TP_NMNAT1;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_Primary_TP_NMNAT1.csv','WriteVariableNames',0);
+FluxDiff.GeneToRxnList = GeneToRxnList;
+FluxDiff_Primary_TP_NMNAT1 = FluxDiff;
+clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_Primary_TP_NMNAT1
+
+%% ASPA (443)
+[~, GeneToRxnList, iAstro_Primary_TP_ASPA] = downregulateGene(iAstro_Primary_TP, '443');
+model_a = iAstro_Primary_TP;
+model_b = iAstro_Primary_TP_ASPA;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_Primary_TP_ASPA.csv','WriteVariableNames',0);
+FluxDiff.GeneToRxnList = GeneToRxnList;
+FluxDiff_Primary_TP_ASPA = FluxDiff;
+clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_Primary_TP_ASPA
+
+%% SLC5A8 (160728)
+[~, GeneToRxnList, iAstro_Primary_TP_SLC5A8] = downregulateGene(iAstro_Primary_TP, '160728');
+model_a = iAstro_Primary_TP;
+model_b = iAstro_Primary_TP_SLC5A8;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_Primary_TP_SLC5A8.csv','WriteVariableNames',0);
+FluxDiff.GeneToRxnList = GeneToRxnList;
+FluxDiff_Primary_TP_SLC5A8 = FluxDiff;
+clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_Primary_TP_SLC5A8
+
+%% PLA2G12A (81579)
+[~, GeneToRxnList, iAstro_Primary_TP_PLA2G12A] = downregulateGene(iAstro_Primary_TP, '81579');
+model_a = iAstro_Primary_TP;
+model_b = iAstro_Primary_TP_PLA2G12A;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_Primary_TP_PLA2G12A.csv','WriteVariableNames',0);
+FluxDiff.GeneToRxnList = GeneToRxnList;
+FluxDiff_Primary_TP_PLA2G12A = FluxDiff;
+clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_Primary_TP_PLA2G12A
+
+%% SLC29A2 (3177)
+[~, GeneToRxnList, iAstro_Primary_TP_SLC29A2] = downregulateGene(iAstro_Primary_TP, '3177');
+model_a = iAstro_Primary_TP;
+model_b = iAstro_Primary_TP_SLC29A2;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_Primary_TP_SLC29A2.csv','WriteVariableNames',0);
+FluxDiff.GeneToRxnList = GeneToRxnList;
+FluxDiff_Primary_TP_SLC29A2 = FluxDiff;
+clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_Primary_TP_SLC29A2
+
+%% COASY (80347)
+[~, GeneToRxnList, iAstro_Primary_TP_COASY] = downregulateGene(iAstro_Primary_TP, '80347');
+model_a = iAstro_Primary_TP;
+model_b = iAstro_Primary_TP_COASY;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_Primary_TP_COASY.csv','WriteVariableNames',0);
+FluxDiff.GeneToRxnList = GeneToRxnList;
+FluxDiff_Primary_TP_COASY = FluxDiff;
+clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_Primary_TP_COASY
+
+%% SYNJ2 (8871)
+[~, GeneToRxnList, iAstro_Primary_TP_SYNJ2] = downregulateGene(iAstro_Primary_TP, '8871');
+model_a = iAstro_Primary_TP;
+model_b = iAstro_Primary_TP_SYNJ2;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_Primary_TP_SYNJ2.csv','WriteVariableNames',0);
+FluxDiff.GeneToRxnList = GeneToRxnList;
+FluxDiff_Primary_TP_SYNJ2 = FluxDiff;
+clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_Primary_TP_SYNJ2
+
+%% PYGL (5836)
+[~, GeneToRxnList, iAstro_Primary_TP_PYGL] = downregulateGene(iAstro_Primary_TP, '5836');
+model_a = iAstro_Primary_TP;
+model_b = iAstro_Primary_TP_PYGL;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_Primary_TP_PYGL.csv','WriteVariableNames',0);
+FluxDiff.GeneToRxnList = GeneToRxnList;
+FluxDiff_Primary_TP_PYGL = FluxDiff;
+clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_Primary_TP_PYGL
+
+%% NEU1 (4758)
+[~, GeneToRxnList, iAstro_Primary_TP_NEU1] = downregulateGene(iAstro_Primary_TP, '4758');
+model_a = iAstro_Primary_TP;
+model_b = iAstro_Primary_TP_NEU1;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_Primary_TP_NEU1.csv','WriteVariableNames',0);
+FluxDiff.GeneToRxnList = GeneToRxnList;
+FluxDiff_Primary_TP_NEU1 = FluxDiff;
+clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_Primary_TP_NEU1
+
+%% UGT2A1 (10941)
+[~, GeneToRxnList, iAstro_Primary_TP_UGT2A1] = downregulateGene(iAstro_Primary_TP, '10941');
+model_a = iAstro_Primary_TP;
+model_b = iAstro_Primary_TP_UGT2A1;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_Primary_TP_UGT2A1.csv','WriteVariableNames',0);
+FluxDiff.GeneToRxnList = GeneToRxnList;
+FluxDiff_Primary_TP_UGT2A1 = FluxDiff;
+clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_Primary_TP_UGT2A1
+
+%% SLC22A9 (114571)
+[~, GeneToRxnList, iAstro_Primary_TP_SLC22A9] = downregulateGene(iAstro_Primary_TP, '114571');
+model_a = iAstro_Primary_TP;
+model_b = iAstro_Primary_TP_SLC22A9;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_Primary_TP_SLC22A9.csv','WriteVariableNames',0);
+FluxDiff.GeneToRxnList = GeneToRxnList;
+FluxDiff_Primary_TP_SLC22A9 = FluxDiff;
+clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_Primary_TP_SLC22A9
+
 %% iAstro_iPS_Ctrl_TP
 %% CKMT2 (1160)
 [~, GeneToRxnList, iAstro_iPS_Ctrl_TP_CKMT2] = downregulateGene(iAstro_iPS_Ctrl_TP, '1160');
@@ -149,6 +292,149 @@ writetable(T,'FSR_iPS_Ctrl_TP_PLCB4.csv','WriteVariableNames',0);
 FluxDiff.GeneToRxnList = GeneToRxnList;
 FluxDiff_iPS_Ctrl_TP_PLCB4 = FluxDiff;
 clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_iPS_Ctrl_TP_PLCB4
+
+%% NMNAT1 (64802)
+[~, GeneToRxnList, iAstro_iPS_Ctrl_TP_NMNAT1] = downregulateGene(iAstro_iPS_Ctrl_TP, '64802');
+model_a = iAstro_iPS_Ctrl_TP;
+model_b = iAstro_iPS_Ctrl_TP_NMNAT1;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_iPS_Ctrl_TP_NMNAT1.csv','WriteVariableNames',0);
+FluxDiff.GeneToRxnList = GeneToRxnList;
+FluxDiff_iPS_Ctrl_TP_NMNAT1 = FluxDiff;
+clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_iPS_Ctrl_TP_NMNAT1
+
+%% ASPA (443)
+[~, GeneToRxnList, iAstro_iPS_Ctrl_TP_ASPA] = downregulateGene(iAstro_iPS_Ctrl_TP, '443');
+model_a = iAstro_iPS_Ctrl_TP;
+model_b = iAstro_iPS_Ctrl_TP_ASPA;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_iPS_Ctrl_TP_ASPA.csv','WriteVariableNames',0);
+FluxDiff.GeneToRxnList = GeneToRxnList;
+FluxDiff_iPS_Ctrl_TP_ASPA = FluxDiff;
+clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_iPS_Ctrl_TP_ASPA
+
+%% SLC5A8 (160728)
+[~, GeneToRxnList, iAstro_iPS_Ctrl_TP_SLC5A8] = downregulateGene(iAstro_iPS_Ctrl_TP, '160728');
+model_a = iAstro_iPS_Ctrl_TP;
+model_b = iAstro_iPS_Ctrl_TP_SLC5A8;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_iPS_Ctrl_TP_SLC5A8.csv','WriteVariableNames',0);
+FluxDiff.GeneToRxnList = GeneToRxnList;
+FluxDiff_iPS_Ctrl_TP_SLC5A8 = FluxDiff;
+clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_iPS_Ctrl_TP_SLC5A8
+
+%% PLA2G12A (81579)
+[~, GeneToRxnList, iAstro_iPS_Ctrl_TP_PLA2G12A] = downregulateGene(iAstro_iPS_Ctrl_TP, '81579');
+model_a = iAstro_iPS_Ctrl_TP;
+model_b = iAstro_iPS_Ctrl_TP_PLA2G12A;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_iPS_Ctrl_TP_PLA2G12A.csv','WriteVariableNames',0);
+FluxDiff.GeneToRxnList = GeneToRxnList;
+FluxDiff_iPS_Ctrl_TP_PLA2G12A = FluxDiff;
+clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_iPS_Ctrl_TP_PLA2G12A
+
+%% SLC29A2 (3177)
+[~, GeneToRxnList, iAstro_iPS_Ctrl_TP_SLC29A2] = downregulateGene(iAstro_iPS_Ctrl_TP, '3177');
+model_a = iAstro_iPS_Ctrl_TP;
+model_b = iAstro_iPS_Ctrl_TP_SLC29A2;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_iPS_Ctrl_TP_SLC29A2.csv','WriteVariableNames',0);
+FluxDiff.GeneToRxnList = GeneToRxnList;
+FluxDiff_iPS_Ctrl_TP_SLC29A2 = FluxDiff;
+clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_iPS_Ctrl_TP_SLC29A2
+
+%% COASY (80347)
+[~, GeneToRxnList, iAstro_iPS_Ctrl_TP_COASY] = downregulateGene(iAstro_iPS_Ctrl_TP, '80347');
+model_a = iAstro_iPS_Ctrl_TP;
+model_b = iAstro_iPS_Ctrl_TP_COASY;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_iPS_Ctrl_TP_COASY.csv','WriteVariableNames',0);
+FluxDiff.GeneToRxnList = GeneToRxnList;
+FluxDiff_iPS_Ctrl_TP_COASY = FluxDiff;
+clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_iPS_Ctrl_TP_COASY
+
+%% SYNJ2 (8871)
+[~, GeneToRxnList, iAstro_iPS_Ctrl_TP_SYNJ2] = downregulateGene(iAstro_iPS_Ctrl_TP, '8871');
+model_a = iAstro_iPS_Ctrl_TP;
+model_b = iAstro_iPS_Ctrl_TP_SYNJ2;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_iPS_Ctrl_TP_SYNJ2.csv','WriteVariableNames',0);
+FluxDiff.GeneToRxnList = GeneToRxnList;
+FluxDiff_iPS_Ctrl_TP_SYNJ2 = FluxDiff;
+clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_iPS_Ctrl_TP_SYNJ2
+
+%% PYGL (5836)
+[~, GeneToRxnList, iAstro_iPS_Ctrl_TP_PYGL] = downregulateGene(iAstro_iPS_Ctrl_TP, '5836');
+model_a = iAstro_iPS_Ctrl_TP;
+model_b = iAstro_iPS_Ctrl_TP_PYGL;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_iPS_Ctrl_TP_PYGL.csv','WriteVariableNames',0);
+FluxDiff.GeneToRxnList = GeneToRxnList;
+FluxDiff_iPS_Ctrl_TP_PYGL = FluxDiff;
+clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_iPS_Ctrl_TP_PYGL
+
+%% NEU1 (4758)
+[~, GeneToRxnList, iAstro_iPS_Ctrl_TP_NEU1] = downregulateGene(iAstro_iPS_Ctrl_TP, '4758');
+model_a = iAstro_iPS_Ctrl_TP;
+model_b = iAstro_iPS_Ctrl_TP_NEU1;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_iPS_Ctrl_TP_NEU1.csv','WriteVariableNames',0);
+FluxDiff.GeneToRxnList = GeneToRxnList;
+FluxDiff_iPS_Ctrl_TP_NEU1 = FluxDiff;
+clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_iPS_Ctrl_TP_NEU1
+
+%% UGT2A1 (10941)
+[~, GeneToRxnList, iAstro_iPS_Ctrl_TP_UGT2A1] = downregulateGene(iAstro_iPS_Ctrl_TP, '10941');
+model_a = iAstro_iPS_Ctrl_TP;
+model_b = iAstro_iPS_Ctrl_TP_UGT2A1;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_iPS_Ctrl_TP_UGT2A1.csv','WriteVariableNames',0);
+FluxDiff.GeneToRxnList = GeneToRxnList;
+FluxDiff_iPS_Ctrl_TP_UGT2A1 = FluxDiff;
+clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_iPS_Ctrl_TP_UGT2A1
+
+%% SLC22A9 (114571)
+[~, GeneToRxnList, iAstro_iPS_Ctrl_TP_SLC22A9] = downregulateGene(iAstro_iPS_Ctrl_TP, '114571');
+model_a = iAstro_iPS_Ctrl_TP;
+model_b = iAstro_iPS_Ctrl_TP_SLC22A9;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_iPS_Ctrl_TP_SLC22A9.csv','WriteVariableNames',0);
+FluxDiff.GeneToRxnList = GeneToRxnList;
+FluxDiff_iPS_Ctrl_TP_SLC22A9 = FluxDiff;
+clear model_a model_b dat m T FluxDiff GeneToRxnList iAstro_iPS_Ctrl_TP_SLC22A9
 
 %% save data to mat file
 save('iAstro_FluxDiff_GeneList_TP.mat'); 
