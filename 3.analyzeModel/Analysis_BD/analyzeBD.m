61e723013c3a8bc48dc2b4bad79e614ec2c05f21
anin90 <anin90@gmail.com>
2022-03-17 12:43:26 +0530

    Fix bad repo

--- 3.analyzeModel/Analysis_BD/analyzeBD.m
+++ 3.analyzeModel/Analysis_BD/analyzeBD.m
@@ -44,42 +44,76 @@ writetable(T,'FSR_iAstro_iPS_Ctrl_TP_abs_vs_iAstro_iPS_BD_NR_TP_abs.csv','WriteV
 FluxDiff_iPS_Ctrl_TP_abs_vs_iPS_BD_NR_TP_abs = FluxDiff;
 clear model_a model_b dat m T FluxDiff
 
-%% iAstro_iPS_TP_norm
-%% iAstro_iPS_Ctrl_TP_norm vs iAstro_iPS_BD_TP_norm
-model_a = iAstro_iPS_Ctrl_TP_norm;
-model_b = iAstro_iPS_BD_TP_norm;
+%% iAstro_iPS_TP_norm_t1
+%% iAstro_iPS_Ctrl_TP_norm_t1 vs iAstro_iPS_BD_TP_norm_t1
+model_a = iAstro_iPS_Ctrl_TP_norm_t1;
+model_b = iAstro_iPS_BD_TP_norm_t1;
 [FluxDiff] = compareCbModel(model_a, model_b);
 dat = FluxDiff;
 m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
 T = table(m);
-writetable(T,'FSR_iAstro_iPS_Ctrl_TP_norm_vs_iAstro_iPS_BD_TP_norm.csv','WriteVariableNames', true, 'Delimiter','\t');
-FluxDiff_iPS_Ctrl_TP_norm_vs_iPS_BD_TP_norm = FluxDiff;
+writetable(T,'FSR_iAstro_iPS_Ctrl_TP_norm_t1_vs_iAstro_iPS_BD_TP_norm_t1.csv','WriteVariableNames', true, 'Delimiter','\t');
+FluxDiff_iPS_Ctrl_TP_norm_t1_vs_iPS_BD_TP_norm_t1 = FluxDiff;
 clear model_a model_b dat m T FluxDiff
 
-%% iAstro_iPS_Ctrl_TP_norm vs iAstro_iPS_BD_R_TP_norm
-model_a = iAstro_iPS_Ctrl_TP_norm;
-model_b = iAstro_iPS_BD_R_TP_norm;
+%% iAstro_iPS_Ctrl_TP_norm_t1 vs iAstro_iPS_BD_R_TP_norm_t1
+model_a = iAstro_iPS_Ctrl_TP_norm_t1;
+model_b = iAstro_iPS_BD_R_TP_norm_t1;
 [FluxDiff] = compareCbModel(model_a, model_b);
 dat = FluxDiff;
 m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
 T = table(m);
-writetable(T,'FSR_iAstro_iPS_Ctrl_TP_norm_vs_iAstro_iPS_BD_R_TP_norm.csv','WriteVariableNames', true, 'Delimiter','\t');
-FluxDiff_iPS_Ctrl_TP_norm_vs_iPS_BD_R_TP_norm = FluxDiff;
+writetable(T,'FSR_iAstro_iPS_Ctrl_TP_norm_t1_vs_iAstro_iPS_BD_R_TP_norm_t1.csv','WriteVariableNames', true, 'Delimiter','\t');
+FluxDiff_iPS_Ctrl_TP_norm_t1_vs_iPS_BD_R_TP_norm_t1 = FluxDiff;
 clear model_a model_b dat m T FluxDiff
 
-%% iAstro_iPS_Ctrl_TP_norm vs iAstro_iPS_BD_NR_TP_norm
-model_a = iAstro_iPS_Ctrl_TP_norm;
-model_b = iAstro_iPS_BD_NR_TP_norm;
+%% iAstro_iPS_Ctrl_TP_norm_t1 vs iAstro_iPS_BD_NR_TP_norm_t1
+model_a = iAstro_iPS_Ctrl_TP_norm_t1;
+model_b = iAstro_iPS_BD_NR_TP_norm_t1;
 [FluxDiff] = compareCbModel(model_a, model_b);
 dat = FluxDiff;
 m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
 T = table(m);
-writetable(T,'FSR_iAstro_iPS_Ctrl_TP_norm_vs_iAstro_iPS_BD_NR_TP_norm.csv','WriteVariableNames', true, 'Delimiter','\t');
-FluxDiff_iPS_Ctrl_TP_norm_vs_iPS_BD_NR_TP_norm = FluxDiff;
+writetable(T,'FSR_iAstro_iPS_Ctrl_TP_norm_t1_vs_iAstro_iPS_BD_NR_TP_norm_t1.csv','WriteVariableNames', true, 'Delimiter','\t');
+FluxDiff_iPS_Ctrl_TP_norm_t1_vs_iPS_BD_NR_TP_norm_t1 = FluxDiff;
+clear model_a model_b dat m T FluxDiff
+
+%% iAstro_iPS_TP_norm_t2
+%% iAstro_iPS_Ctrl_TP_norm_t2 vs iAstro_iPS_BD_TP_norm_t2
+model_a = iAstro_iPS_Ctrl_TP_norm_t2;
+model_b = iAstro_iPS_BD_TP_norm_t2;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_iAstro_iPS_Ctrl_TP_norm_t2_vs_iAstro_iPS_BD_TP_norm_t2.csv','WriteVariableNames', true, 'Delimiter','\t');
+FluxDiff_iPS_Ctrl_TP_norm_t2_vs_iPS_BD_TP_norm_t2 = FluxDiff;
+clear model_a model_b dat m T FluxDiff
+
+%% iAstro_iPS_Ctrl_TP_norm_t2 vs iAstro_iPS_BD_R_TP_norm_t2
+model_a = iAstro_iPS_Ctrl_TP_norm_t2;
+model_b = iAstro_iPS_BD_R_TP_norm_t2;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_iAstro_iPS_Ctrl_TP_norm_t2_vs_iAstro_iPS_BD_R_TP_norm_t2.csv','WriteVariableNames', true, 'Delimiter','\t');
+FluxDiff_iPS_Ctrl_TP_norm_t2_vs_iPS_BD_R_TP_norm_t2 = FluxDiff;
+clear model_a model_b dat m T FluxDiff
+
+%% iAstro_iPS_Ctrl_TP_norm_t2 vs iAstro_iPS_BD_NR_TP_norm_t2
+model_a = iAstro_iPS_Ctrl_TP_norm_t2;
+model_b = iAstro_iPS_BD_NR_TP_norm_t2;
+[FluxDiff] = compareCbModel(model_a, model_b);
+dat = FluxDiff;
+m = vertcat(union(dat.fluxGoneHigh.FSr_significant_H, dat.fluxGoneLow.FSr_significant_L));
+T = table(m);
+writetable(T,'FSR_iAstro_iPS_Ctrl_TP_norm_t2_vs_iAstro_iPS_BD_NR_TP_norm_t2.csv','WriteVariableNames', true, 'Delimiter','\t');
+FluxDiff_iPS_Ctrl_TP_norm_t2_vs_iPS_BD_NR_TP_norm_t2 = FluxDiff;
 clear model_a model_b dat m T FluxDiff
 
 %%
-clearvars -except FluxDiff_iPS_Ctrl_TP_abs_vs_iPS_BD_TP_abs FluxDiff_iPS_Ctrl_TP_abs_vs_iPS_BD_R_TP_abs FluxDiff_iPS_Ctrl_TP_abs_vs_iPS_BD_NR_TP_abs FluxDiff_iPS_Ctrl_TP_norm_vs_iPS_BD_TP_norm FluxDiff_iPS_Ctrl_TP_norm_vs_iPS_BD_R_TP_norm FluxDiff_iPS_Ctrl_TP_norm_vs_iPS_BD_NR_TP_norm 
+clearvars -except FluxDiff_iPS_Ctrl_TP_abs_vs_iPS_BD_TP_abs FluxDiff_iPS_Ctrl_TP_abs_vs_iPS_BD_R_TP_abs FluxDiff_iPS_Ctrl_TP_abs_vs_iPS_BD_NR_TP_abs FluxDiff_iPS_Ctrl_TP_norm_t1_vs_iPS_BD_TP_norm_t1 FluxDiff_iPS_Ctrl_TP_norm_t1_vs_iPS_BD_R_TP_norm_t1 FluxDiff_iPS_Ctrl_TP_norm_t1_vs_iPS_BD_NR_TP_norm_t1 FluxDiff_iPS_Ctrl_TP_norm_t2_vs_iPS_BD_TP_norm_t2 FluxDiff_iPS_Ctrl_TP_norm_t2_vs_iPS_BD_R_TP_norm_t2 FluxDiff_iPS_Ctrl_TP_norm_t2_vs_iPS_BD_NR_TP_norm_t2
 
 %% save data to mat file
 save('iAstro_FluxDiff_BD.mat'); 
--- 3.analyzeModel/Annotations/AnnotateRxnSubsystems/annotateRxnSubsystems.m
+++ 3.analyzeModel/Annotations/AnnotateRxnSubsystems/annotateRxnSubsystems.m
@@ -15,11 +15,18 @@ writetable(table(iAstro_iPS_BD_TP_abs.rxns, [iAstro_iPS_BD_TP_abs.subSystems{:}]
 writetable(table(iAstro_iPS_BD_R_TP_abs.rxns, [iAstro_iPS_BD_R_TP_abs.subSystems{:}]', 'VariableNames',{'Rxn','SubSystem'}), 'Rxns_iPS_BD_R_TP_abs.csv', 'WriteVariableNames', true, 'Delimiter', '\t');
 writetable(table(iAstro_iPS_BD_NR_TP_abs.rxns, [iAstro_iPS_BD_NR_TP_abs.subSystems{:}]', 'VariableNames',{'Rxn','SubSystem'}), 'Rxns_iPS_BD_NR_TP_abs.csv', 'WriteVariableNames', true, 'Delimiter', '\t');
 
-% norm
-writetable(table(iAstro_Primary_TP_norm.rxns, [iAstro_Primary_TP_norm.subSystems{:}]', 'VariableNames',{'Rxn','SubSystem'}), 'Rxns_Primary_TP_norm.csv', 'WriteVariableNames', true, 'Delimiter', '\t');
-writetable(table(iAstro_iPS_Ctrl_TP_norm.rxns, [iAstro_iPS_Ctrl_TP_norm.subSystems{:}]', 'VariableNames',{'Rxn','SubSystem'}), 'Rxns_iPS_Ctrl_TP_norm.csv', 'WriteVariableNames', true, 'Delimiter', '\t');
-writetable(table(iAstro_iPS_BD_TP_norm.rxns, [iAstro_iPS_BD_TP_norm.subSystems{:}]', 'VariableNames',{'Rxn','SubSystem'}), 'Rxns_iPS_BD_TP_norm.csv', 'WriteVariableNames', true, 'Delimiter', '\t');
-writetable(table(iAstro_iPS_BD_R_TP_norm.rxns, [iAstro_iPS_BD_R_TP_norm.subSystems{:}]', 'VariableNames',{'Rxn','SubSystem'}), 'Rxns_iPS_BD_R_TP_norm.csv', 'WriteVariableNames', true, 'Delimiter', '\t');
-writetable(table(iAstro_iPS_BD_NR_TP_norm.rxns, [iAstro_iPS_BD_NR_TP_norm.subSystems{:}]', 'VariableNames',{'Rxn','SubSystem'}), 'Rxns_iPS_BD_NR_TP_norm.csv', 'WriteVariableNames', true, 'Delimiter', '\t');
+% norm_t1
+writetable(table(iAstro_Primary_TP_norm_t1.rxns, [iAstro_Primary_TP_norm_t1.subSystems{:}]', 'VariableNames',{'Rxn','SubSystem'}), 'Rxns_Primary_TP_norm_t1.csv', 'WriteVariableNames', true, 'Delimiter', '\t');
+writetable(table(iAstro_iPS_Ctrl_TP_norm_t1.rxns, [iAstro_iPS_Ctrl_TP_norm_t1.subSystems{:}]', 'VariableNames',{'Rxn','SubSystem'}), 'Rxns_iPS_Ctrl_TP_norm_t1.csv', 'WriteVariableNames', true, 'Delimiter', '\t');
+writetable(table(iAstro_iPS_BD_TP_norm_t1.rxns, [iAstro_iPS_BD_TP_norm_t1.subSystems{:}]', 'VariableNames',{'Rxn','SubSystem'}), 'Rxns_iPS_BD_TP_norm_t1.csv', 'WriteVariableNames', true, 'Delimiter', '\t');
+writetable(table(iAstro_iPS_BD_R_TP_norm_t1.rxns, [iAstro_iPS_BD_R_TP_norm_t1.subSystems{:}]', 'VariableNames',{'Rxn','SubSystem'}), 'Rxns_iPS_BD_R_TP_norm_t1.csv', 'WriteVariableNames', true, 'Delimiter', '\t');
+writetable(table(iAstro_iPS_BD_NR_TP_norm_t1.rxns, [iAstro_iPS_BD_NR_TP_norm_t1.subSystems{:}]', 'VariableNames',{'Rxn','SubSystem'}), 'Rxns_iPS_BD_NR_TP_norm_t1.csv', 'WriteVariableNames', true, 'Delimiter', '\t');
+
+% norm_t2
+writetable(table(iAstro_Primary_TP_norm_t2.rxns, [iAstro_Primary_TP_norm_t2.subSystems{:}]', 'VariableNames',{'Rxn','SubSystem'}), 'Rxns_Primary_TP_norm_t2.csv', 'WriteVariableNames', true, 'Delimiter', '\t');
+writetable(table(iAstro_iPS_Ctrl_TP_norm_t2.rxns, [iAstro_iPS_Ctrl_TP_norm_t2.subSystems{:}]', 'VariableNames',{'Rxn','SubSystem'}), 'Rxns_iPS_Ctrl_TP_norm_t2.csv', 'WriteVariableNames', true, 'Delimiter', '\t');
+writetable(table(iAstro_iPS_BD_TP_norm_t2.rxns, [iAstro_iPS_BD_TP_norm_t2.subSystems{:}]', 'VariableNames',{'Rxn','SubSystem'}), 'Rxns_iPS_BD_TP_norm_t2.csv', 'WriteVariableNames', true, 'Delimiter', '\t');
+writetable(table(iAstro_iPS_BD_R_TP_norm_t2.rxns, [iAstro_iPS_BD_R_TP_norm_t2.subSystems{:}]', 'VariableNames',{'Rxn','SubSystem'}), 'Rxns_iPS_BD_R_TP_norm_t2.csv', 'WriteVariableNames', true, 'Delimiter', '\t');
+writetable(table(iAstro_iPS_BD_NR_TP_norm_t2.rxns, [iAstro_iPS_BD_NR_TP_norm_t2.subSystems{:}]', 'VariableNames',{'Rxn','SubSystem'}), 'Rxns_iPS_BD_NR_TP_norm_t2.csv', 'WriteVariableNames', true, 'Delimiter', '\t');
 
 %%
